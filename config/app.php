59cdd85a4d99fc88d05262f033902b7f4a9b6ff1
jr-public <jorgeruiz.kj@gmail.com>
2025-06-06 14:35:36 +0200

    Removed hardcoded email links

--- config/app.php
+++ config/app.php
@@ -1,15 +1,18 @@
 <?php
 
 $fake_app_env = 'development';
+$url = 'http://localhost';
+
 
 return [
     'name' 		=> getenv('APP_PHP_NAME'),
     'version' 	=> '0.0.0',
     'env'		=> $fake_app_env,
-    'debug' 	=> $fake_app_env == 'development'
+    'debug' 	=> $fake_app_env == 'development',
     // 'env'		=> getenv('APP_ENV'),
     // 'debug' 	=> getenv('APP_ENV') == 'development'
-    // 'url' => 'http://localhost',
+    'url' => $url,
+    'confirm_path' => $url . '/confirm.php',
     // 'timezone' => 'UTC',
     // 'locale' => 'en',
 ];
--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use App\Service\ConfigService;
 
 class EmailService {
     private string $host;
@@ -13,7 +14,9 @@ class EmailService {
     private string $fromEmail;
     private string $fromName;
 
-    public function __construct() {
+    public function __construct(
+        private readonly ConfigService $config
+    ) {
         $this->host = getenv('MAIL_HOST');
         $this->port = (int) getenv('MAIL_PORT');
         $this->username = getenv('MAIL_USERNAME');
@@ -59,29 +62,33 @@ class EmailService {
 
 
     public function sendActivationEmail(string $email, string $username, string $token): bool {
-        $activationUrl = "http://localhost/activation.php?token=" . urlencode($token);
-        
+        $confirmUrl = $this->config->get('confirm_path').'?'.http_build_query([
+            'action' => 'activate',
+            'token' => $token
+        ]);
         $subject = "Activate your account";
         $body = "
             <h2>Welcome {$username}!</h2>
             <p>Please click the link below to activate your account:</p>
-            <p><a href='{$activationUrl}'>Activate Account</a></p>
-            <p>Or copy this link: {$activationUrl}</p>
+            <p><a href='{$confirmUrl}'>Activate Account</a></p>
+            <p>Or copy this link: {$confirmUrl}</p>
         ";
 
         return $this->send($email, $subject, $body, $username);
     }
 
     public function sendPasswordResetEmail(string $email, string $username, string $token): bool {
-        $resetUrl = "http://localhost/password_reset.php?token=" . urlencode($token);
-        
+        $confirmUrl = $this->config->get('confirm_path').'?'.http_build_query([
+            'action' => 'reset',
+            'token' => $token
+        ]);
         $subject = "Reset your password";
         $body = "
             <h2>Password Reset Request</h2>
             <p>Hi {$username},</p>
             <p>Click the link below to reset your password:</p>
-            <p><a href='{$resetUrl}'>Reset Password</a></p>
-            <p>Or copy this link: {$resetUrl}</p>
+            <p><a href='{$confirmUrl}'>Reset Password</a></p>
+            <p>Or copy this link: {$confirmUrl}</p>
             <p>This link will expire in 1 hour.</p>
         ";
 
